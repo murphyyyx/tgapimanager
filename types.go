38b7b8bb02ec9b20d39940eacb84405132ed95ed
Anton <anton.satirin@.com>
2024-09-15 12:59:02 +0200

    user.from fixes

--- types.go
+++ types.go
@@ -15,11 +15,64 @@ type Update struct {
 	Message  *Message `json:"message,omitempty"`
 }
 
+// User represents a Telegram user or bot.
 type User struct {
-	ID        int    `json:"id"`
-	IsBot     bool   `json:"is_bot"`
+	// ID is a unique identifier for this user or bot
+	ID int64 `json:"id"`
+	// IsBot true, if this user is a bot
+	//
+	// optional
+	IsBot bool `json:"is_bot,omitempty"`
+	// FirstName user's or bot's first name
 	FirstName string `json:"first_name"`
-	Username  string `json:"username"`
+	// LastName user's or bot's last name
+	//
+	// optional
+	LastName string `json:"last_name,omitempty"`
+	// UserName user's or bot's username
+	//
+	// optional
+	UserName string `json:"username,omitempty"`
+	// LanguageCode IETF language tag of the user's language
+	// more info: https://en.wikipedia.org/wiki/IETF_language_tag
+	//
+	// optional
+	LanguageCode string `json:"language_code,omitempty"`
+	// CanJoinGroups is true, if the bot can be invited to groups.
+	// Returned only in getMe.
+	//
+	// optional
+	CanJoinGroups bool `json:"can_join_groups,omitempty"`
+	// CanReadAllGroupMessages is true, if privacy mode is disabled for the bot.
+	// Returned only in getMe.
+	//
+	// optional
+	CanReadAllGroupMessages bool `json:"can_read_all_group_messages,omitempty"`
+	// SupportsInlineQueries is true, if the bot supports inline queries.
+	// Returned only in getMe.
+	//
+	// optional
+	SupportsInlineQueries bool `json:"supports_inline_queries,omitempty"`
+}
+
+// String displays a simple text version of a user.
+//
+// It is normally a user's username, but falls back to a first/last
+// name as available.
+func (u *User) String() string {
+	if u == nil {
+		return ""
+	}
+	if u.UserName != "" {
+		return u.UserName
+	}
+
+	name := u.FirstName
+	if u.LastName != "" {
+		name += " " + u.LastName
+	}
+
+	return name
 }
 
 type Chat struct {
